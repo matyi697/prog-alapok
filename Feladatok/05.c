03a6f0554fb90b6fff72555b4578758f842a4107
matyi697 <wittmann.matyas@gmail.com>
2024-11-12 17:37:42 +0100

    new file:   Feladatok/05.c

---
+++ Feladatok/05.c
@@ -0,0 +1,26 @@
+/*
+Számok fordított sorrendben történő kiírása
+
+Készíts egy programot, amely pozitív egész számokat kér be a felhasználótól, és fordított sorrendben írja ki azokat.
+A program addig folytatja a számok beolvasását, amíg a felhasználó be nem ír egy 0 értéket.
+A 0 nem kerül bele a kiírt eredménybe; csak a beolvasás végét jelzi.
+
+Feladat részletei:
+    A program folyamatosan kérjen be egész számokat a felhasználótól.
+    Csak a pozitív számokat mentse el egy tömbbe.
+    Amint a felhasználó 0-t ír be, a program fejezze be a beolvasást.
+    A program a beolvasott számokat fordított sorrendben írja ki.
+    Azaz az utolsóként megadott pozitív számot írja ki először,
+    és így tovább az elsőként megadott számig.
+
+Például:
+    INPUT: 5 3 8 0
+    OUTPUT: 8 3 5
+
+Megjegyzés:
+
+    Ha a felhasználó azonnal 0-t ad meg, a program nem ír ki semmit,
+    mivel nincs eltárolt pozitív szám.
+    A program maximálisan 100 számot tud eltárolni.
+    Ha a felhasználó ennél többet adna meg, csak az első 100 szám kerül mentésre.
+*/
