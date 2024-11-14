98d7f308916f785c40310d08e5a470c7c419c939
matyi697 <wittmann.matyas@gmail.com>
2024-11-14 18:03:57 +0100

    modified:   Feladatok/03.c

--- Feladatok/03.c
+++ Feladatok/03.c
@@ -9,3 +9,29 @@ INPUT: 4                INPUT: 5
        oooo             o   o
                         ooooo
 */
+
+#include <stdio.h>
+
+int main () {
+    int n = 0;
+    scanf("%d", &n);
+    
+    for (int i = 0; i < n; i++)     //elso sor kiirasa (full "o" betuk, n darab)
+        printf("o");
+    printf("\n");
+    
+    for (int i = 0; i < n-2; i++) {     //vegig megyunk az olyan sorokon ahol az elso es utolso karakter van csak
+        printf("o");                    //elso karakter
+        
+        for (int m = 0; m < n-2; m++)   //a koztuk levo spacek
+            printf(" ");
+        
+        printf("o\n");                  //sor vegen az utolso karakter es ujsorra ugrunk
+    }
+    
+    for (int i = 0; i < n; i++)         //utolso sor kiirasa (full "o" betuk, n darab)
+        printf("o");
+    
+    printf("\n");
+    return 0;
+}
